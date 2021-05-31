9ec20604ca8d746c12617ae8b03ebeffad799bbe
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-05-31 15:49:39 +0300

    make the change

--- src/test/java/models/PingPongTest.java
+++ src/test/java/models/PingPongTest.java
@@ -6,11 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PingPongTest {
+//    @Test
+//    public void name() {
+//    }
+
     @Test
-    public void runPingPong_countUpToOne_ArrayList() {
+    public void runPingPong_replaceMultiplesOf3_ArrayList() {
         PingPong testPingPong = new PingPong();
         List<Object> expectedOutput = new ArrayList<Object>();
         expectedOutput.add(1);
-        assertEquals(expectedOutput, testPingPong.runPingPong(1));
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        expectedOutput.add("ping");
+        expectedOutput.add(7);
+        expectedOutput.add(8);
+        expectedOutput.add("ping");
+        expectedOutput.add("pong");
+        expectedOutput.add(11);
+        expectedOutput.add("ping");
+        expectedOutput.add(13);
+        expectedOutput.add(14);
+        expectedOutput.add("pingpong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(15));
     }
 }
